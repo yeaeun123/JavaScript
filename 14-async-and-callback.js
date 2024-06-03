310aae3fea76b0f7c54cf3e2b847685af538f3ec
yeaeun123 <yeaeun0125@gmail.com>
2024-06-03 18:02:36 +0900

    240603오후

--- 14-async-and-callback.js
+++ 14-async-and-callback.js
@@ -50,6 +50,40 @@ function testLogicPromise() {
     })
     .catch(reason => {  // Promise 내부에서 reject 함수로 전달해 준 값 (rejected 상태로)
         console.log("PROMISE reject: ", reason);
-    })
+    });
 };
-testLogicPromise();
+// testLogicPromise();
+
+// async / await
+// -> Promise를 손쉽게 사용하기 위한 키워드
+// async 함수는 비동기 처리를 위해 Promise를 생성한다.
+async function f() {
+    console.log("async function");
+    return 1;
+}
+// console.log(f());
+
+// 가상 시나리오:
+// 네트워크 통신을 통해 데이터를 가져온다는 가정
+function fetchData() {
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+        resolve("서버로부터 가져온 데이터");
+        }, 2000);
+    })
+}
+
+async function processData() {
+    console.log("데이터를 가져오는 중...");
+    try {
+        // await 키워드 : Promise가 해결될 때까지 코드를 일시 중지시킴
+        //                 Promise가 해소되면 Primise의 결과 값을 반환
+        // await 키워드 함수 사용을 위해 async가 붙어 있어야 함
+        const data = await fetchData();
+        console.log("가져온 데이터: ", data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+processData();
